efad187f6e7cd2e27e8d28a9a30dd0e6e4c224ea
thing-zoo <cathy4025@gmail.com>
2022-10-25 17:19:52 +0200

    make new_post_page scrollable and add image picker

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MySpotApp extends StatelessWidget {
           return GetMaterialApp(
             debugShowCheckedModeBanner: false,
             title: 'MySpot',
-            initialRoute: "/SignUp",
+            initialRoute: "/NewPost",
             getPages: [
               // GetPage(name: "/Load", page: () => LoadingPage(), transition: Transition.fadeIn, binding: BindingsBuilder(
               //         () => Get.lazyPut<clientController>(() => clientController()))),
