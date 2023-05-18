a623b0a54562e37d3d9a2976fe860cd044408972
HyeonJunSong <music9801@naver.com>
2023-05-18 21:52:50 +0900

    other bugs, versions fixed

--- android/app/src/main/kotlin/com/example/myspot/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myspot/MainActivity.kt
@@ -2,5 +2,12 @@ package com.example.myspot
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", "transparent")
+        super.onCreate(savedInstanceState)
+    }
 }
